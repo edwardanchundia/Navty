cadeaedf1a8280aff6a4b9389eacb7580eb29304
Edward Anchundia <edward.anchundia@gmail.com>
2017-02-28 14:55:06 -0500

    initial project

---
+++ Navty/Navty/APIRequestManager.swift
@@ -0,0 +1,26 @@
+//
+//  APIRequestManager.swift
+//  Navty
+//
+//  Created by Edward Anchundia on 2/28/17.
+//  Copyright © 2017 Edward Anchundia. All rights reserved.
+//
+
+import Foundation
+
+class APIRequestManager {
+    static let manager = APIRequestManager()
+    private init() {}
+    
+    func getData(endPoint: String, callback: @escaping (Data?) -> Void) {
+        guard let myURL = URL(string: endPoint) else { return }
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        session.dataTask(with: myURL) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil {
+                print("Error during session: \(error)")
+            }
+            guard let validData = data else { return }
+            callback(validData)
+            }.resume()
+    }
+}
